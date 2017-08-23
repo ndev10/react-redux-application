329a228d0cbfd25a3327e7336527ff9f938e8f25
devang.ghugharawala <devang.ghugharawala@nitorinfotech.net>
2017-08-23 22:15:04 +0530

    React Router 2 with redux-form 4

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,15 @@
+import {FETCH_POSTS,FETCH_POST} from '../actions/index'
+
+const INTIAL_STATE = {all:[],post:null};
+
+export default function(state = INTIAL_STATE,action) {
+ 
+  switch(action.type) {
+      case FETCH_POST : 
+        return {...state,post:action.payload.data}
+      case FETCH_POSTS : 
+        return {...state,all:action.payload.data}
+      default : 
+        return state;
+  }
+}
